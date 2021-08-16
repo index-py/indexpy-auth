301b5b128e901ffb17c778ba47158cebfb23b5aa
abersheeran <me@abersheeran.com>
2021-08-16 23:54:19 +0800

    -

--- indexpy_auth/middlewares.py
+++ indexpy_auth/middlewares.py
@@ -21,27 +21,27 @@ class NeedAuthentication(Generic[T_Response], metaclass=abc.ABCMeta):
 
     def __init__(self, endpoint: Callable[[], Awaitable[T_Response]]) -> None:
         self.endpoint = endpoint
+        describe_response(
+            "401",
+            content={
+                "application/json": {
+                    "schema": {
+                        "type": "object",
+                        "properties": {
+                            "message": {"type": "string"},
+                        },
+                    }
+                }
+            },
+            headers={
+                "WWW-Authenticate": {
+                    "description": "Bearer token",
+                    "schema": {"type": "string"},
+                }
+            },
+        )(endpoint)
         describe_extra_docs(endpoint, {"security": [{"BearerAuth": []}]})
 
-    @describe_response(
-        "401",
-        content={
-            "application/json": {
-                "schema": {
-                    "type": "object",
-                    "properties": {
-                        "message": {"type": "string"},
-                    },
-                }
-            }
-        },
-        headers={
-            "WWW-Authenticate": {
-                "description": "Bearer token",
-                "schema": {"type": "string"},
-            }
-        },
-    )
     async def __call__(self) -> T_Response:
         authorization = request.headers.get("Authorization", None)
         if authorization is None:
